f921a43e6c4f7ed450c96d3330f111848f425b40
Facundo <facundomoriconi19@gmail.com>
2024-12-23 09:45:21 -0300

    commit

--- frontend/src/components/DevTree.tsx
+++ frontend/src/components/DevTree.tsx
@@ -1,13 +1,23 @@
 import NavigationTabs from "./NavigationTabs"
 import { Link, Outlet } from "react-router-dom"
 import { Toaster } from "sonner";
-import { User } from "../types";
+import { SocialNetwork, User } from "../types";
+import { useEffect, useState } from "react";
+import DevTreelink from "./DevTreeLink";
 
 type DevTreeProps = {
     data: User
 }
 
 export default function DevTree({data}: DevTreeProps){
+    
+    const [enabledLinks, setEnabledLinks] = useState<SocialNetwork[]>(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled))
+
+
+    useEffect(()=>{
+        setEnabledLinks(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled))
+    }, [data])
+
     return (
         <>
             <header className="bg-slate-800 py-5">
@@ -51,6 +61,18 @@ export default function DevTree({data}: DevTreeProps){
                             }
 
                             <p className="text-center text-lg font-black text-white">{data.description}</p>
+
+                            <p className="text-white">Iconos</p>
+
+                            <div className="mt-20 flex flex-col gap-5">
+                                {enabledLinks.map(link => (
+                                    <DevTreelink 
+                                    key={link.name}
+                                    link={link}
+                                    />
+                                ))}
+                            </div>
+
                         </div>
                     </div>
                 </main>
--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,6 +1,88 @@
+import { useEffect, useState } from "react"
+import { social } from "../data/social"
+import DevTreeInput from "../components/DevTreeInput"
+import { isValidUrl } from "../utils"
+import { toast } from "sonner"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { updateProfile } from "../api/DevTreeAPI"
+import { SocialNetwork, User } from "../types"
 
 export default function LinkTreeView() {
+    const [devTreeLinks, setDevTreeLinks] = useState(social)
+    
+    const queryClient = useQueryClient()
+    const user : User = queryClient.getQueryData(['user'])!
+    const { mutate } = useMutation({
+        mutationFn: updateProfile,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: () => {
+            toast.success('Actualizado Correctamente')
+        }
+    })
+
+    useEffect(()=>{
+        const updatedData = devTreeLinks.map( item => {
+            const userLink = JSON.parse(user.links).find((link: SocialNetwork) => link.name === item.name)
+            if(userLink){
+                return {...item, url: userLink.url, enabled: userLink.enabled} 
+            }
+            return item
+
+        })
+        
+        setDevTreeLinks(updatedData)
+    }, [])
+
+    const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
+        const updateLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value} : link)
+        console.log(updateLinks)
+        setDevTreeLinks(updateLinks)
+
+        queryClient.setQueryData(['user'], (prevData: User)=>{
+            return{
+                ...prevData,
+                links: JSON.stringify(updateLinks)
+            }
+        })
+    }
+
+    const handleEnabledLink = (socialNetwork : string) =>{
+        const updateLinks = devTreeLinks.map(link => {
+           if (link.name === socialNetwork) {
+                if(isValidUrl(link.url)){
+                    return {...link, enabled: !link.enabled}
+                } else{
+                    toast.error('URL no Válida')
+                }
+           } 
+           return link
+        })
+        setDevTreeLinks(updateLinks)
+
+        queryClient.setQueryData(['user'], (prevData: User)=>{
+            return{
+                ...prevData,
+                links: JSON.stringify(updateLinks)
+            }
+        })
+    }
+
     return(
-        <div>LinkTree</div>
+        <div className="space-y-5">
+            {devTreeLinks.map( item => (
+                <DevTreeInput
+                  key={item.name}
+                  item={item}
+                  handleUrlChange={handleUrlChange}
+                  handleEnabledLink={handleEnabledLink}
+                />
+            ))}
+            <button
+                className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded font-bold"
+                onClick={() => mutate(user)}
+            >Guardar Cambios</button>
+        </div>
     )
 }
